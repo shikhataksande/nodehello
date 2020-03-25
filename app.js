9ee45342f044474bb8d08e3714c6dca35bcdb27c
Shikha Taksande <Shikha.Taksande@arup.com>
2020-03-25 12:58:57 +1100

    changed to kill process

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
+  
+  process.kill(process.pid, 'SIGTERM')
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+process.on('SIGTERM', () => {
+    server.close(() => {
+      console.log('Process terminated')
+    })
+  })
